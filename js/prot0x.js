05476d741fbed3fbb835cb21a0f81aa4d222ac7b
DEFLAT Coin <47925155+deflatcoin@users.noreply.github.com>
2019-04-01 22:44:56 -0300

    0x Connect

--- js/prot0x.js
+++ js/prot0x.js
@@ -16,7 +16,7 @@ function connect0x() {
                                                 feeRecipient: '0x4d717d48BB24Af867B5efC91b282264Aae83cFa6',
                                                 feePercentage: 0.01,
                                             },
-											availableAssetDatas: ['0xf47261b0000000000000000000000000e1e0db951844e7fb727574d7daca68d1c5d1525b'],
+					    //availableAssetDatas: ['0xf47261b0000000000000000000000000e1e0db951844e7fb727574d7daca68d1c5d1525b'],
                                             defaultSelectedAssetData: '0xf47261b0000000000000000000000000e1e0db951844e7fb727574d7daca68d1c5d1525b',
                                         },											
 							            'body',								
