c61d21f0beb41a4cfb573ab2da954de0134c22cc
DEFLAT Coin <47925155+deflatcoin@users.noreply.github.com>
2019-04-01 23:04:57 -0300

    DEFLAT Over 0x Protocol Connection

--- js/prot0x.js
+++ js/prot0x.js
@@ -1,7 +1,7 @@
 function connect0x() {
   		    zeroExInstant.render(
 										{
-												orderSource: 'https://api.radarrelay.com/0x/v2/',
+												orderSource: 'http://api-v2.ledgerdex.com/sra/v2',
 												additionalAssetMetaDataMap: {                                                
 												'0xf47261b0000000000000000000000000e1e0db951844e7fb727574d7daca68d1c5d1525b': {
                                                 assetProxyId: '0xf47261b0', 
@@ -16,7 +16,7 @@ function connect0x() {
                                                 feeRecipient: '0x4d717d48BB24Af867B5efC91b282264Aae83cFa6',
                                                 feePercentage: 0.01,
                                             },
-					    //availableAssetDatas: ['0xf47261b0000000000000000000000000e1e0db951844e7fb727574d7daca68d1c5d1525b'],
+					    availableAssetDatas: ['0xf47261b0000000000000000000000000e1e0db951844e7fb727574d7daca68d1c5d1525b'],
                                             defaultSelectedAssetData: '0xf47261b0000000000000000000000000e1e0db951844e7fb727574d7daca68d1c5d1525b',
                                         },											
 							            'body',								
